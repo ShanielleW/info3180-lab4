6efed30b2cb0540ae77e7b4835085b9b20a6cc75
ShanielleW <shanielle.williams@hotmail.com>
2018-02-25 20:17:41 -0500

    Lab Completion

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ This file creates your application.
 """
 import os
 from app import app
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, jsonify
 from werkzeug.utils import secure_filename
 
 
@@ -25,23 +25,22 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
-@app.route('/upload', methods=['POST', 'GET'])
-def upload():
+@app.route('/add-file', methods=['POST', 'GET'])
+def add_file():
     if not session.get('logged_in'):
         abort(401)
 
-    # Instantiate your form class
+    file_folder = app.config['UPLOAD_FOLDER']
 
-    # Validate file upload on submit
     if request.method == 'POST':
-        # Get file data and save to your uploads folder
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(file_folder, filename))
 
-        flash('File Saved', 'success')
+        flash('File Saved')
         return redirect(url_for('home'))
 
-    return render_template('upload.html')
-
+    return render_template('add_file.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -52,30 +51,34 @@ def login():
         else:
             session['logged_in'] = True
             
-            flash('You were logged in', 'success')
-            return redirect(url_for('upload'))
+            flash('You were logged in')
+            return redirect(url_for('add_file'))
     return render_template('login.html', error=error)
-
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    flash('You were logged out', 'success')
+    flash('You were logged out')
     return redirect(url_for('home'))
+    
+@app.route('/filelisting')
+def file_listing():
+    """ Lists uploaded files """
+    if not session.get('logged_in'):
+        abort(401)
+    filelist = [] 
+    rootdir = os.getcwd() 
+    
+    for subdir, dirs, files in os.walk(rootdir + '/app/static/uploads'):
+        for file in files:
+            filelist.append(file)
+            
+    return render_template('uploads.html', filelist=filelist)
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
-# Flash errors from the form if validation fails
-def flash_errors(form):
-    for field, errors in form.errors.items():
-        for error in errors:
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-), 'danger')
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -102,4 +105,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="8080")
+    app.run(debug=True,host="0.0.0.0",port="8080")
